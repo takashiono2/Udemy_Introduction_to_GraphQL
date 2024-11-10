fad875e2a9b56c013184ecd425892aa117b8297c
takashiono2 <takashiono710@gmail.com>
2024-11-10 14:48:33 +0900

    section6

--- src/server.js
+++ src/server.js
@@ -2,42 +2,48 @@ const { gql, ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 //HackerNewsの1つ1つの投稿
-let links = [
-  {
-    id: "link-0",
-    description: "GraphQLチュートリアルをUdemyで学ぶ",
-    url: "www.udemy-graphql-tutorial.com",
-  },
-];
+// let links = [
+//   {
+//     id: "link-0",
+//     description: "GraphQLチュートリアルをUdemyで学ぶ",
+//     url: "www.udemy-graphql-tutorial.com",
+//   },
+// ];
 
 //リゾルバ関数
 const resolvers = {
   Query: {
     info: () => 'HackerNews クローン',
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
   },
 
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
-    }
-  }
+    post: (parent, args, context) => {
+      const newlink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        }
+      });
+      return newlink;
+    },
+  },
 };
 
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
+  context: {
+    prisma,
+  }
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバー起動中・・・`));
