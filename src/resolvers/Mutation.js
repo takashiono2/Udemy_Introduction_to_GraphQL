774d1ec49cc2f5f90b418b97e95c62b95b9caa75
takashiono2 <takashiono710@gmail.com>
2024-11-30 19:00:29 +0900

    section7_31

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcriptjs");
+const jwt = require("jsonwebtoken");
 
+APP_SECRET = "Graphql";
 
 // ユーザーの新規登録のリゾルバ
 async function signup(parent, args, context) {
@@ -12,4 +14,33 @@ async function signup(parent, args, context) {
       password,
     },
   });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+}
+
+// ユーザーログイン
+async function login(parent, args, context) {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  });
+  if (!user) {
+    throw new Error("そのようなユーザーは存在しません");
+  }
+  //パスワードの比較
+  const valid = await bcrypt.compare(args.password, user.password);
+  if (!valid) {
+    throw new Error("無効なパスワードです");
+  }
+  //パスワードが正しい時
+  const token = jwt.sign({ useId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
 }
