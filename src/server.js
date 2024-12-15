e5c22bb99169dda45533d3187c4a38cc6b05a843
takashiono2 <takashiono710@gmail.com>
2024-12-15 17:57:25 +0900

    section7_37

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const { PrismaClient } = require("@prisma/client");
+const { getUserId } = require("/utils");
 
 const prisma = new PrismaClient();
 
@@ -41,9 +42,13 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
-  context: {
-    prisma,
-  }
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      UserId: req && req.headers.authorization ? getUserId(req) : null,
+    }
+  },
 });
 
 server.listen().then(({ url }) => console.log(`${url}でサーバー起動中・・・`));
